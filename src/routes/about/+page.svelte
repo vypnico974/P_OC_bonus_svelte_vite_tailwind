5bee0c261969a8fb670ac90b21fad1ae245b3e35
Vyplasil Nicolas <nicolas.vyplasil@hotmail.com>
2023-07-08 08:10:27 +0400

    first commit

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,47 @@
+<!-- <svelte:head>
+	<title>About</title>
+	<meta name="description" content="About this app" />
+</svelte:head>
+
+<div class="text-column">
+	<h1>About this app</h1>
+
+	<p>
+		This is a <a href="https://kit.svelte.dev">SvelteKit</a> app. You can make your own by typing the
+		following into your command line and following the prompts:
+	</p>
+
+	<pre>npm create svelte@latest</pre>
+
+	<p>
+		The page you're looking at is purely static HTML, with no client-side interactivity needed.
+		Because of that, we don't need to load any JavaScript. Try viewing the page's source, or opening
+		the devtools network panel and reloading.
+	</p>
+
+	<p>
+		The <a href="/sverdle">Sverdle</a> page illustrates SvelteKit's data loading and form handling. Try
+		using it with JavaScript disabled!
+	</p>
+</div> -->
+
+
+<h1 class="text-3xl font-bold underline">
+	Hello world!
+  </h1>
+
+  <div class="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
+    <p class="text-3xl text-gray-700 font-bold mb-5">
+		Hello world!
+    </p>
+    <p class="text-gray-500 text-lg">
+       SvelteKit and Tailwind CSS in action
+    </p>
+</div>
+  
+  <style lang="postcss">
+	:global(html) {
+	  background-color: theme(colors.gray.100);
+	}
+  </style>
+
